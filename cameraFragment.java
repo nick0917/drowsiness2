69b221db4e12e39d2d3222126a27aaeb24df311b
nick0917 <nick0917@naver.com>
2018-05-24 13:01:38 +0900

    Initial commit

---
+++ cameraFragment.java
@@ -0,0 +1,29 @@
+package com.example.andriod.drowsiness;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class cameraFragment extends Fragment {
+    String TAG = "cameraFragment";
+
+    public static cameraFragment newInstance() {
+        return new cameraFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.camera,null);
+
+        return view;
+    }
+}
